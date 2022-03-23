e79ee3ca6997e064e6e5b5f3a5534cfbb968051a
Felipetds <felipe.teixeiradasilva13@gmail.com>
2022-03-22 22:58:10 -0300

    make it better

--- main.py
+++ main.py
@@ -3,24 +3,8 @@ import requests
 
 app = Flask(__name__)
 
-
-def teste(name):
-    p1 = '00000000'
-    if len(p1) == len(name):
-        teste = 'corrreto'
-        return teste
-    else:
-        teste = 'erro'
-        return teste
-
 @app.route("/", methods=['GET', 'POST'])
 def homepage():
-    if request.method == 'POST':
-        name = request.form['name']
-    else:
-        name = ''
-
-
     return render_template("homepage.html", name=name)
 
 if __name__ == "__main__":
